3c84f1a0b96ff6a7db08f3e8e5babd72c12a2790
Jan Chyb <jan.chyb.99@gmail.com>
2022-03-07 15:46:33 +0100

    Set correct comment syntax in stdlibs generated via scaladoc
    Wiki syntax was set for scala 2.13 stdlib and markdown was set for scala
    3 stdlib.

--- project/Build.scala
+++ project/Build.scala
@@ -1836,6 +1836,10 @@ object ScaladocConfigs {
       .add(ExternalMappings(List(javaExternalMapping)))
       .add(DocRootContent(docRootFile.toString))
       .add(CommentSyntax("wiki"))
+      .add(CommentSyntaxOverrides(List(
+        s"${dottyLibRoot}=markdown",
+        s"${stdLibRoot}=wiki"
+      )))
       .add(VersionsDictionaryUrl("https://scala-lang.org/api/versions.json"))
       .add(DocumentSyntheticTypes(true))
       .add(SnippetCompiler(List(
